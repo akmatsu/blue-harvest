0e65c22b4379e95228c7694b9e2ee0332acf43d9
Freedom Evenden <free.eve@pm.me>
2024-05-23 11:16:44 -0800

    feat: users can now report images

--- resources/js/Components/Form/index.ts
+++ resources/js/Components/Form/index.ts
@@ -1,3 +1,4 @@
 export { default as PasswordInput } from './PasswordInput.vue';
 export { default as DragAndDropInput } from './DragAndDropInput.vue';
 export { default as SearchBar } from './SearchBar.vue';
+export { default as ReportDialog } from './ReportDialog.vue';
