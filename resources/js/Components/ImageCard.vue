19a560a14c64837b8803f976bacac1c5aac3e547
Freedom Evenden <free.eve@pm.me>
2024-05-29 13:42:54 -0800

    feat: optimized browse view images

--- resources/js/Components/ImageCard.vue
+++ resources/js/Components/ImageCard.vue
@@ -1,19 +1,24 @@
 <script lang="ts" setup>
 import { Image } from '@/types';
 import { LinkCard } from './containers';
+import { useDisplay } from 'vuetify';
 
-defineProps<{
+const props = defineProps<{
   image: Image;
 }>();
+
+const url = computed(() => {
+  if (props.image.optimized_images) {
+    const img = props.image.optimized_images.find((im) => im.size === 'medium');
+    if (img) return img?.url;
+  }
+  return props.image.url;
+});
 </script>
 
 <template>
   <LinkCard link="images.view" :params="{ id: image.id }" preserve-state>
-    <v-img
-      :src="image.url"
-      cover
-      :aspect-ratio="image.width / image.height"
-    ></v-img>
+    <v-img :src="url" cover :aspect-ratio="image.width / image.height"></v-img>
     <v-card-text v-if="image.tags" class="px-2 py-0">
       <v-chip-group column>
         <v-chip
