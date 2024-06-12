ff33d639e2b50b3680000fca67fa3e1149c4ca3e
f3ve <free.eve@pm.me>
2024-06-12 15:10:30 -0800

    configurin aws s3

--- config/filesystems.php
+++ config/filesystems.php
@@ -42,13 +42,16 @@ return [
       'throw' => false,
     ],
 
-    'azure' => [
-      'driver' => 'azure',
-      'name' => env('AZURE_STORAGE_NAME'),
-      'key' => env('AZURE_STORAGE_KEY'),
-      'container' => env('AZURE_STORAGE_CONTAINER'),
-      'url' => env('AZURE_STORAGE_URL'), // Optional, if you want to generate URLs
-      'prefix' => env('AZURE_STORAGE_PREFIX', ''), // Optional, if you want to add a prefix
+    's3' => [
+      'driver' => 's3',
+      'key' => env('AWS_ACCESS_KEY_ID'),
+      'secret' => env('AWS_SECRET_ACCESS_KEY'),
+      'region' => env('AWS_DEFAULT_REGION'),
+      'bucket' => env('AWS_BUCKET'),
+      'url' => env('AWS_URL'),
+      'endpoint' => env('AWS_ENDPOINT'),
+      'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+      'throw' => false,
     ],
   ],
 
