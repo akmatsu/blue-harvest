fa4501be51f69da018c749ef0fba1c72c4aed40e
f3ve <free.eve@pm.me>
2024-06-13 13:31:25 -0800

    created prod database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,42 @@ class DatabaseSeeder extends Seeder
    */
   public function run(): void
   {
-    User::factory(10)->create();
+    // User::factory(10)->create();
 
-    User::factory()->create([
-      'name' => 'Test User',
-      'email' => 'test@example.com',
+    // User::factory()->create([
+    //   'name' => 'Test User',
+    //   'email' => 'test@example.com',
+    // ]);
+
+    $adminRole = Role::updateOrCreate([
+      'name' => env('ADMIN_NAME'),
+      'password' => Hash::make(env('ADMIN_PASSWORD')),
     ]);
+
+    // Define permissions
+    $permissions = [
+      'view users',
+      'edit users',
+      'delete users',
+      'edit images',
+      'delete images',
+      'view flags',
+      'edit flags',
+      'delete flags',
+    ];
+
+    // Create and assign permissions to the admin role
+    foreach ($permissions as $permission) {
+      $perm = Permission::updateOrCreate(['name' => $permission]);
+      $adminRole->givePermissionTo($perm);
+    }
+
+    // Assign the admin role to a specific user
+    $admin = User::find(13); // Assuming the first user is the admin
+    if ($admin) {
+      $admin->assignRole('admin');
+    }
+
+    $this->call([TagSeeder::class]);
   }
 }
