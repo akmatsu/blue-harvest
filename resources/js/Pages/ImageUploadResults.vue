49735f21658ea04a48c1ad34a0700e26bc9296b4
f3ve <free.eve@pm.me>
2024-06-21 15:54:56 -0800

    feat: added buttons and alert to top of upload page. Closes #65

--- resources/js/Pages/ImageUploadResults.vue
+++ resources/js/Pages/ImageUploadResults.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { LinkBtn } from '@/Components';
 import { CoreLayout } from '@/Layouts';
 import { Image, Tag } from '@/types';
 import { UploadResultCard } from './Upload/components';
@@ -10,6 +11,16 @@ defineProps<{
 </script>
 <template>
   <CoreLayout title="Upload Results">
+    <v-alert icon="mdi-information" color="success">
+      You're images have successfully uploaded and are still being processed.
+      You will receive a notification when you images are finished processing
+      <template #append>
+        <div class="d-flex flex-column" style="gap: 4px">
+          <LinkBtn link="upload.index"> Upload more images </LinkBtn>
+          <LinkBtn link="index" color="primary">Back to app</LinkBtn>
+        </div>
+      </template>
+    </v-alert>
     <v-list>
       <div v-for="(image, index) in images" :key="image.id">
         <UploadResultCard :image="image" :tags="tags" />
