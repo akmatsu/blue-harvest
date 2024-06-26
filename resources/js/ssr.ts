5093c5cb322a05cab13de3db57c249cccd2970b7
f3ve <free.eve@pm.me>
2024-06-26 15:52:01 -0800

    fixed page titles for pages with no title

--- resources/js/ssr.ts
+++ resources/js/ssr.ts
@@ -1,8 +1,8 @@
-import { createSSRApp, h, DefineComponent } from 'vue';
-import { renderToString } from '@vue/server-renderer';
 import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
+import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+import { createSSRApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -11,7 +11,7 @@ createServer((page) =>
   createInertiaApp({
     page,
     render: renderToString,
-    title: (title) => `${title} - ${appName}`,
+    title: (title) => (title ? `${title} - ${appName}` : appName),
     resolve: (name) =>
       resolvePageComponent(
         `./Pages/${name}.vue`,
