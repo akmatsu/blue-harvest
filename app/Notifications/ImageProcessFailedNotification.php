089f792731cf3e662654ee6efb7434f030583bf6
f3ve <free.eve@pm.me>
2024-06-25 17:00:34 -0800

    wip: improving queues

--- app/Notifications/ImageProcessFailedNotification.php
+++ app/Notifications/ImageProcessFailedNotification.php
@@ -21,6 +21,7 @@ class ImageProcessFailedNotification extends Notification implements ShouldQueue
   public function __construct(Image $image)
   {
     $this->image = $image;
+    $this->onQueue('notifications');
   }
 
   /**
--- app/Notifications/ImageProcessedNotification.php
+++ app/Notifications/ImageProcessedNotification.php
@@ -21,6 +21,7 @@ class ImageProcessedNotification extends Notification implements ShouldQueue
   public function __construct(Image $image)
   {
     $this->image = $image;
+    $this->onQueue('notifications');
   }
 
   /**
