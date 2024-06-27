90450b50ec2c1d5ed711bdd7a66d72deb34f1049
f3ve <free.eve@pm.me>
2024-06-27 12:32:11 -0800

    removed unused code

--- database/seeders/UpdateImageAttribution.php
+++ database/seeders/UpdateImageAttribution.php
@@ -4,9 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Image;
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 class UpdateImageAttribution extends Seeder
 {
