fbc75318730c4027bc28f50dddd043bfd7528626
f3ve <free.eve@pm.me>
2024-12-10 14:57:27 -0900

    update apps and dependencies

--- database/migrations/2024_06_21_212847_make_description_nullable_in_images.php
+++ database/migrations/2024_06_21_212847_make_description_nullable_in_images.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
@@ -20,6 +21,10 @@ return new class extends Migration {
    */
   public function down(): void
   {
+    DB::table('images')
+      ->whereNull('description')
+      ->update(['description' => 'No Description']);
+
     Schema::table('images', function (Blueprint $table) {
       $table->string('description')->change();
     });
