674d453678101b01ed5cabe7b7054372eaf33d4b
f3ve <free.eve@pm.me>
2025-01-16 15:49:02 -0900

    feat: created UI for bulk editing

--- resources/js/Pages/Browse.vue
+++ resources/js/Pages/Browse.vue
@@ -29,6 +29,12 @@ const { exec: execSearch } = useRequest(handleSearch, {
     <MasonryGrid class="mb-4">
       <ImageCard v-for="image in scrollImages" :key="image.id" :image="image" />
     </MasonryGrid>
+    <div
+      v-if="scrollImages?.length === 0"
+      class="text-center w-full text-gray-500"
+    >
+      <p>No Images found.</p>
+    </div>
     <div class="d-flex justify-center">
       <CorePagination :pagination="images" :search="search" />
     </div>
